0a56bfec3a937a0c90a61c3d568459e082e3ab4d
reaper <reaper>
2023-02-12 19:40:08 +0300

    Вычисление и вывод треугольного числа

--- Homework.java
+++ Homework.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Homework {
     private static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-    int n = getNumberByUser("Введите количество элементов ");
+    int n = getNumberByUser("Введите количество элементов: ");
     System.out.println("Треугольное число = " + Triangle(n)+ "\n");
     }
     public static int getNumberByUser(String text) {
